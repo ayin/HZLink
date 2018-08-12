30b44a4827e041dba84cbc285c2cfe4917f3acdb
yindongyang <yindongyang@kuaishou.com>
2018-08-12 19:36:40 +0800

    HZLink release

---
+++ js/default.thm_aff0a4f3.js
@@ -0,0 +1,512 @@
+window.skins={};
+                function __extends(d, b) {
+                    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+                        function __() {
+                            this.constructor = d;
+                        }
+                    __.prototype = b.prototype;
+                    d.prototype = new __();
+                };
+                window.generateEUI = {};
+                generateEUI.paths = {};
+                generateEUI.styles = undefined;
+                generateEUI.skins = {"eui.Button":"resource/eui_skins/ButtonSkin.exml","eui.CheckBox":"resource/eui_skins/CheckBoxSkin.exml","eui.HScrollBar":"resource/eui_skins/HScrollBarSkin.exml","eui.ProgressBar":"resource/eui_skins/ProgressBarSkin.exml","eui.Scroller":"resource/eui_skins/ScrollerSkin.exml","eui.VScrollBar":"resource/eui_skins/VScrollBarSkin.exml","Dialog":"resource/eui_skins/DialogSkin.exml","BtnFacebook":"resource/skins/BtnFacebook.exml"};generateEUI.paths['resource/skins/BtnFacebook.exml'] = window.skins.ButtonSkin = (function (_super) {
+	__extends(ButtonSkin, _super);
+	function ButtonSkin() {
+		_super.call(this);
+		this.skinParts = [];
+		
+		this.height = 80;
+		this.width = 80;
+		this.elementsContent = [this._Image1_i()];
+		this.states = [
+			new eui.State ("up",
+				[
+				])
+			,
+			new eui.State ("down",
+				[
+				])
+			,
+			new eui.State ("disabled",
+				[
+				])
+		];
+	}
+	var _proto = ButtonSkin.prototype;
+
+	_proto._Image1_i = function () {
+		var t = new eui.Image();
+		t.percentHeight = 100;
+		t.source = "face";
+		t.percentWidth = 100;
+		return t;
+	};
+	return ButtonSkin;
+})(eui.Skin);generateEUI.paths['resource/skins/BtnHome.exml'] = window.skins.ButtonSkin = (function (_super) {
+	__extends(ButtonSkin, _super);
+	function ButtonSkin() {
+		_super.call(this);
+		this.skinParts = [];
+		
+		this.height = 80;
+		this.width = 127;
+		this.elementsContent = [this._Image1_i()];
+		this.states = [
+			new eui.State ("up",
+				[
+				])
+			,
+			new eui.State ("down",
+				[
+					new eui.SetProperty("_Image1","source","home1")
+				])
+			,
+			new eui.State ("disabled",
+				[
+				])
+		];
+	}
+	var _proto = ButtonSkin.prototype;
+
+	_proto._Image1_i = function () {
+		var t = new eui.Image();
+		this._Image1 = t;
+		t.percentHeight = 100;
+		t.source = "home";
+		t.percentWidth = 100;
+		return t;
+	};
+	return ButtonSkin;
+})(eui.Skin);generateEUI.paths['resource/skins/BtnPause.exml'] = window.skins.ButtonSkin = (function (_super) {
+	__extends(ButtonSkin, _super);
+	function ButtonSkin() {
+		_super.call(this);
+		this.skinParts = [];
+		
+		this.height = 60;
+		this.width = 60;
+		this.elementsContent = [this._Image1_i()];
+		this.states = [
+			new eui.State ("up",
+				[
+				])
+			,
+			new eui.State ("down",
+				[
+					new eui.SetProperty("_Image1","source","pause1")
+				])
+			,
+			new eui.State ("disabled",
+				[
+				])
+		];
+	}
+	var _proto = ButtonSkin.prototype;
+
+	_proto._Image1_i = function () {
+		var t = new eui.Image();
+		this._Image1 = t;
+		t.percentHeight = 100;
+		t.source = "pause";
+		t.percentWidth = 100;
+		return t;
+	};
+	return ButtonSkin;
+})(eui.Skin);generateEUI.paths['resource/skins/BtnRestart.exml'] = window.skins.ButtonSkin = (function (_super) {
+	__extends(ButtonSkin, _super);
+	function ButtonSkin() {
+		_super.call(this);
+		this.skinParts = [];
+		
+		this.height = 80;
+		this.width = 127;
+		this.elementsContent = [this._Image1_i()];
+		this.states = [
+			new eui.State ("up",
+				[
+				])
+			,
+			new eui.State ("down",
+				[
+					new eui.SetProperty("_Image1","source","restart1")
+				])
+			,
+			new eui.State ("disabled",
+				[
+				])
+		];
+	}
+	var _proto = ButtonSkin.prototype;
+
+	_proto._Image1_i = function () {
+		var t = new eui.Image();
+		this._Image1 = t;
+		t.percentHeight = 100;
+		t.source = "restart";
+		t.percentWidth = 100;
+		return t;
+	};
+	return ButtonSkin;
+})(eui.Skin);generateEUI.paths['resource/eui_skins/ButtonSkin.exml'] = window.skins.ButtonSkin = (function (_super) {
+	__extends(ButtonSkin, _super);
+	function ButtonSkin() {
+		_super.call(this);
+		this.skinParts = ["labelDisplay","iconDisplay"];
+		
+		this.minHeight = 30;
+		this.minWidth = 30;
+		this.elementsContent = [this._Image1_i(),this.labelDisplay_i(),this.iconDisplay_i()];
+		this.states = [
+			new eui.State ("up",
+				[
+					new eui.SetProperty("_Image1","alpha",0.8)
+				])
+			,
+			new eui.State ("down",
+				[
+					new eui.SetProperty("_Image1","source","piece_bg_se")
+				])
+			,
+			new eui.State ("disabled",
+				[
+					new eui.SetProperty("_Image1","alpha",0.5)
+				])
+		];
+	}
+	var _proto = ButtonSkin.prototype;
+
+	_proto._Image1_i = function () {
+		var t = new eui.Image();
+		this._Image1 = t;
+		t.percentHeight = 100;
+		t.scale9Grid = new egret.Rectangle(1,3,8,8);
+		t.source = "piece_bg";
+		t.percentWidth = 100;
+		return t;
+	};
+	_proto.labelDisplay_i = function () {
+		var t = new eui.Label();
+		this.labelDisplay = t;
+		t.bottom = 8;
+		t.left = 8;
+		t.right = 8;
+		t.size = 20;
+		t.textAlign = "center";
+		t.textColor = 0x000000;
+		t.top = 8;
+		t.verticalAlign = "middle";
+		return t;
+	};
+	_proto.iconDisplay_i = function () {
+		var t = new eui.Image();
+		this.iconDisplay = t;
+		t.horizontalCenter = 0;
+		t.verticalCenter = 0;
+		return t;
+	};
+	return ButtonSkin;
+})(eui.Skin);generateEUI.paths['resource/eui_skins/CheckBoxSkin.exml'] = window.skins.CheckBoxSkin = (function (_super) {
+	__extends(CheckBoxSkin, _super);
+	function CheckBoxSkin() {
+		_super.call(this);
+		this.skinParts = ["labelDisplay"];
+		
+		this.elementsContent = [this._Group1_i()];
+		this.states = [
+			new eui.State ("up",
+				[
+				])
+			,
+			new eui.State ("down",
+				[
+					new eui.SetProperty("_Image1","alpha",0.7)
+				])
+			,
+			new eui.State ("disabled",
+				[
+					new eui.SetProperty("_Image1","alpha",0.5)
+				])
+			,
+			new eui.State ("upAndSelected",
+				[
+					new eui.SetProperty("_Image1","source","checkbox_select_up_png")
+				])
+			,
+			new eui.State ("downAndSelected",
+				[
+					new eui.SetProperty("_Image1","source","checkbox_select_down_png")
+				])
+			,
+			new eui.State ("disabledAndSelected",
+				[
+					new eui.SetProperty("_Image1","source","checkbox_select_disabled_png")
+				])
+		];
+	}
+	var _proto = CheckBoxSkin.prototype;
+
+	_proto._Group1_i = function () {
+		var t = new eui.Group();
+		t.percentHeight = 100;
+		t.percentWidth = 100;
+		t.layout = this._HorizontalLayout1_i();
+		t.elementsContent = [this._Image1_i(),this.labelDisplay_i()];
+		return t;
+	};
+	_proto._HorizontalLayout1_i = function () {
+		var t = new eui.HorizontalLayout();
+		t.verticalAlign = "middle";
+		return t;
+	};
+	_proto._Image1_i = function () {
+		var t = new eui.Image();
+		this._Image1 = t;
+		t.alpha = 1;
+		t.fillMode = "scale";
+		t.source = "checkbox_unselect_png";
+		return t;
+	};
+	_proto.labelDisplay_i = function () {
+		var t = new eui.Label();
+		this.labelDisplay = t;
+		t.fontFamily = "Tahoma";
+		t.size = 20;
+		t.textAlign = "center";
+		t.textColor = 0x707070;
+		t.verticalAlign = "middle";
+		return t;
+	};
+	return CheckBoxSkin;
+})(eui.Skin);generateEUI.paths['resource/eui_skins/DialogSkin.exml'] = window.skins.DialogSkin = (function (_super) {
+	__extends(DialogSkin, _super);
+	function DialogSkin() {
+		_super.call(this);
+		this.skinParts = ["titleDisplay","header","closeBtn"];
+		
+		this.minHeight = 300;
+		this.minWidth = 400;
+		this.elementsContent = [this._Image1_i(),this.header_i(),this.closeBtn_i()];
+	}
+	var _proto = DialogSkin.prototype;
+
+	_proto._Image1_i = function () {
+		var t = new eui.Image();
+		t.bottom = 0;
+		t.left = 0;
+		t.right = 0;
+		t.scale9Grid = new egret.Rectangle(2,2,12,12);
+		t.source = "border_png";
+		t.top = 0;
+		return t;
+	};
+	_proto.header_i = function () {
+		var t = new eui.Group();
+		this.header = t;
+		t.height = 60;
+		t.left = 0;
+		t.right = 0;
+		t.top = 0;
+		t.elementsContent = [this._Image2_i(),this.titleDisplay_i()];
+		return t;
+	};
+	_proto._Image2_i = function () {
+		var t = new eui.Image();
+		t.bottom = 0;
+		t.left = 0;
+		t.right = 0;
+		t.source = "header_png";
+		t.top = 0;
+		return t;
+	};
+	_proto.titleDisplay_i = function () {
+		var t = new eui.Label();
+		this.titleDisplay = t;
+		t.fontFamily = "Tahoma";
+		t.horizontalCenter = 0;
+		t.size = 40;
+		t.textColor = 0x000000;
+		t.verticalCenter = 0;
+		t.wordWrap = false;
+		return t;
+	};
+	_proto.closeBtn_i = function () {
+		var t = new eui.Image();
+		this.closeBtn = t;
+		t.height = 40;
+		t.right = -10;
+		t.source = "close";
+		t.top = -10;
+		t.width = 40;
+		return t;
+	};
+	return DialogSkin;
+})(eui.Skin);generateEUI.paths['resource/eui_skins/HScrollBarSkin.exml'] = window.skins.HScrollBarSkin = (function (_super) {
+	__extends(HScrollBarSkin, _super);
+	function HScrollBarSkin() {
+		_super.call(this);
+		this.skinParts = ["thumb"];
+		
+		this.minHeight = 8;
+		this.minWidth = 20;
+		this.elementsContent = [this.thumb_i()];
+	}
+	var _proto = HScrollBarSkin.prototype;
+
+	_proto.thumb_i = function () {
+		var t = new eui.Image();
+		this.thumb = t;
+		t.height = 8;
+		t.scale9Grid = new egret.Rectangle(3,3,2,2);
+		t.source = "roundthumb_png";
+		t.verticalCenter = 0;
+		t.width = 30;
+		return t;
+	};
+	return HScrollBarSkin;
+})(eui.Skin);generateEUI.paths['resource/eui_skins/ItemRendererSkin11.exml'] = window.skins.ItemRendererSkin = (function (_super) {
+	__extends(ItemRendererSkin, _super);
+	function ItemRendererSkin() {
+		_super.call(this);
+		this.skinParts = ["labelDisplay"];
+		
+		this.minHeight = 50;
+		this.minWidth = 100;
+		this.elementsContent = [this._Image1_i(),this.labelDisplay_i()];
+		this.states = [
+			new eui.State ("up",
+				[
+				])
+			,
+			new eui.State ("down",
+				[
+					new eui.SetProperty("_Image1","source","button_down_png")
+				])
+			,
+			new eui.State ("disabled",
+				[
+					new eui.SetProperty("_Image1","alpha",0.5)
+				])
+		];
+		
+		eui.Binding.$bindProperties(this, ["hostComponent.data"],[0],this.labelDisplay,"text");
+	}
+	var _proto = ItemRendererSkin.prototype;
+
+	_proto._Image1_i = function () {
+		var t = new eui.Image();
+		this._Image1 = t;
+		t.percentHeight = 100;
+		t.scale9Grid = new egret.Rectangle(1,3,8,8);
+		t.source = "button_up_png";
+		t.percentWidth = 100;
+		return t;
+	};
+	_proto.labelDisplay_i = function () {
+		var t = new eui.Label();
+		this.labelDisplay = t;
+		t.bottom = 8;
+		t.fontFamily = "Tahoma";
+		t.left = 8;
+		t.right = 8;
+		t.size = 20;
+		t.textAlign = "center";
+		t.textColor = 0xFFFFFF;
+		t.top = 8;
+		t.verticalAlign = "middle";
+		return t;
+	};
+	return ItemRendererSkin;
+})(eui.Skin);generateEUI.paths['resource/eui_skins/ProgressBarSkin.exml'] = window.skins.ProgressBarSkin = (function (_super) {
+	__extends(ProgressBarSkin, _super);
+	function ProgressBarSkin() {
+		_super.call(this);
+		this.skinParts = ["thumb","labelDisplay"];
+		
+		this.minHeight = 18;
+		this.minWidth = 30;
+		this.elementsContent = [this._Image1_i(),this.thumb_i(),this.labelDisplay_i()];
+	}
+	var _proto = ProgressBarSkin.prototype;
+
+	_proto._Image1_i = function () {
+		var t = new eui.Image();
+		t.percentHeight = 100;
+		t.scale9Grid = new egret.Rectangle(1,1,4,4);
+		t.source = "track_pb_png";
+		t.verticalCenter = 0;
+		t.percentWidth = 100;
+		return t;
+	};
+	_proto.thumb_i = function () {
+		var t = new eui.Image();
+		this.thumb = t;
+		t.percentHeight = 100;
+		t.source = "thumb_pb_png";
+		t.percentWidth = 100;
+		return t;
+	};
+	_proto.labelDisplay_i = function () {
+		var t = new eui.Label();
+		this.labelDisplay = t;
+		t.fontFamily = "Tahoma";
+		t.horizontalCenter = 0;
+		t.size = 15;
+		t.textAlign = "center";
+		t.textColor = 0x707070;
+		t.verticalAlign = "middle";
+		t.verticalCenter = 0;
+		return t;
+	};
+	return ProgressBarSkin;
+})(eui.Skin);generateEUI.paths['resource/eui_skins/ScrollerSkin.exml'] = window.skins.ScrollerSkin = (function (_super) {
+	__extends(ScrollerSkin, _super);
+	function ScrollerSkin() {
+		_super.call(this);
+		this.skinParts = ["horizontalScrollBar","verticalScrollBar"];
+		
+		this.minHeight = 20;
+		this.minWidth = 20;
+		this.elementsContent = [this.horizontalScrollBar_i(),this.verticalScrollBar_i()];
+	}
+	var _proto = ScrollerSkin.prototype;
+
+	_proto.horizontalScrollBar_i = function () {
+		var t = new eui.HScrollBar();
+		this.horizontalScrollBar = t;
+		t.bottom = 0;
+		t.percentWidth = 100;
+		return t;
+	};
+	_proto.verticalScrollBar_i = function () {
+		var t = new eui.VScrollBar();
+		this.verticalScrollBar = t;
+		t.percentHeight = 100;
+		t.right = 0;
+		return t;
+	};
+	return ScrollerSkin;
+})(eui.Skin);generateEUI.paths['resource/eui_skins/VScrollBarSkin.exml'] = window.skins.VScrollBarSkin = (function (_super) {
+	__extends(VScrollBarSkin, _super);
+	function VScrollBarSkin() {
+		_super.call(this);
+		this.skinParts = ["thumb"];
+		
+		this.minHeight = 20;
+		this.minWidth = 8;
+		this.elementsContent = [this.thumb_i()];
+	}
+	var _proto = VScrollBarSkin.prototype;
+
+	_proto.thumb_i = function () {
+		var t = new eui.Image();
+		this.thumb = t;
+		t.height = 30;
+		t.horizontalCenter = 0;
+		t.scale9Grid = new egret.Rectangle(3,3,2,2);
+		t.source = "roundthumb_png";
+		t.width = 8;
+		return t;
+	};
+	return VScrollBarSkin;
+})(eui.Skin);
